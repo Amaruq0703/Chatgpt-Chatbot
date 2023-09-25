8100865b4bff5857b11b2e6111c6404fc8966868
Aditya Khera <aditya.khera7@gmail.com>
2023-09-25 05:02:52 +0100

    initial commit, created chatbot UI and linked openai chat gpt api

--- backend.py
+++ backend.py
@@ -1,1 +1,16 @@
-import requests
+import openai
+from key import apikey
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = apikey
+
+    def getresponse(self, userinput):
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt = userinput,
+            max_tokens = 4000,
+            temperature = 0.4
+        ).choices[0].text
+        return response
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PyQt6.QtWidgets as pyqt
 import sys
+import backend
 
 class ChatbotWindow(pyqt.QMainWindow):
     def __init__(self):
@@ -8,14 +9,28 @@ class ChatbotWindow(pyqt.QMainWindow):
 
         self.chatarea = pyqt.QTextEdit(self)
         self.chatarea.setGeometry(10, 10, 780, 400)
+        
 
         self.inputfield  = pyqt.QLineEdit(self)
         self.inputfield.setGeometry(10, 470, 780, 50)
 
         self.button = pyqt.QPushButton('Send', self)
         self.button.setGeometry(690, 530, 100, 50)
+        self.button.clicked.connect(self.chat)
 
         self.show()
+
+    def chat(self):
+        
+        userinput = self.inputfield.text().strip()
+        self.chatarea.append(f'<p style="color:#333333">Me: {userinput}</p>')
+        self.inputfield.clear()
+
+        
+        chatter = backend.Chatbot()
+        response = chatter.getresponse(userinput)
+        self.chatarea.append(f'<p style="color:#333333; background-color: #E9E9E9">Bot: {response}</p>')
+
 
 
 app = pyqt.QApplication(sys.argv)
